6c94eba2a14ffccde135d06e532a8f57fee0d359
yunyeoung <dbsdud0823@naver.com>
2020-05-20 14:04:00 +0900

    [20200520]update item detail

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,10 +1,19 @@
-import React from "react"
-import { Breadcrumb, BreadcrumbItem, Row, Col } from "reactstrap"
+import React, { useState } from "react"
+import { Breadcrumb, BreadcrumbItem, Row, Col, Nav, NavItem, TabContent, TabPane, NavLink } from "reactstrap"
 
 import titleimg from "../image/mario.png";
+import classnames from "classnames";
 
 
 const ItemDetail = () =>{
+
+    const [activeTab, setActiveTab] = useState('1');
+
+    const toggle = tab => {
+      if(activeTab !== tab) setActiveTab(tab);
+    }
+  
+
     return(
         <div>
             <Breadcrumb tag="nav" listTag="div">
@@ -20,7 +29,43 @@ const ItemDetail = () =>{
                     item name
                 </Col>
             </Row>
-
+            <div>
+                <Nav tabs>
+                    <NavItem>
+                        <NavLink className={classnames({ active: activeTab === '1' })}
+                                onClick={() => { toggle('1'); }}>
+                            ItemDetail
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink
+                            className={classnames({ active: activeTab === '2' })}
+                            onClick={() => { toggle('2'); }}
+                        >
+                            Notice
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink
+                            className={classnames({ active: activeTab === '3' })}
+                            onClick={() => { toggle('3'); }}
+                        >
+                            QnA
+                        </NavLink>
+                    </NavItem>
+                </Nav>
+                <TabContent activeTab={activeTab}>
+                    <TabPane tabId="1">
+                        item detail information
+                    </TabPane>
+                    <TabPane tabId="2">
+                        notice
+                    </TabPane>
+                    <TabPane tabId="3">
+                        qna
+                    </TabPane>
+                </TabContent>
+            </div>
         </div>
     )
 }
