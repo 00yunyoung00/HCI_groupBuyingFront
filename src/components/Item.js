afa5fa0e113e60182182726cfde7ad3eec9795d8
unknown <kebinkorea@gmail.com>
2020-06-06 02:42:45 +0900

    Item Progress Bar added

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Card, CardImg, CardTitle, CardSubtitle, CardBody } from "reactstrap";
 import { Link } from "react-router-dom";
-
+import {Line} from 'rc-progress';
 import thumbnailImg from "../image/mario.png";
 
 const Item = ({ path, img, name, idx }) => {
@@ -11,6 +11,7 @@ const Item = ({ path, img, name, idx }) => {
                 <Card style={{height:'100%'}}>
                     <CardImg top width="100px" height="160px" src={img} alt="thumbnail image"/>
                     <CardBody>
+                        <Line percent="20" strokeWidth="1" trailWidth="1" strokeColor="red"/>
                         <CardTitle>{name}</CardTitle>
                     </CardBody>
                 </Card>
